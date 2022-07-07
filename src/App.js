18182f872f8dd4f31e4fa5e875713f4606b5f074
Fluffy-Lion <leoncooper31@gmail.com>
2022-07-07 10:12:20 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import "./App.css";
+import { useState } from "react";
+import img1 from "./images/code1.jpeg";
+const App = () => {
+  const [jobs, setJobs] = useState([
+    { role: "junior developer", type: "temporary" },
+    { role: "scrum master", type: "permanent" },
+    { role: "software developer", type: "permanent" },
+  ]);
+  const [selected, setSelected] = useState(false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>welcome to my sight</h1>
+      <div>
+        {jobs.map((job, index) => {
+          return <Card job={job} key={index} />;
+        })}
+      </div>
+      <button onClick={() => setSelected(true)}>add CV</button>
+      {selected && <AddCv />}
+      <div>
+        <img src={img1} />
+      </div>
     </div>
   );
-}
+};
+
+const Card = ({ job }) => {
+  return (
+    <div>
+      <h2>{job.role}</h2>
+      <h3>{job.type}</h3>
+      <button>apply</button>
+    </div>
+  );
+};
+
+const AddCv = () => {
+  return <p>you have submitted your CV</p>;
+};
 
 export default App;
